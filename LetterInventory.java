a1927a4516d8c2534bef1ab735e5dbd99ee02413
Meghan Flaherty <131192974+flaherty-mi@users.noreply.github.com>
2023-05-06 16:33:02 -0700

    Add files via upload

--- LetterInventory.java
+++ LetterInventory.java
@@ -1,8 +1,17 @@
-public class LetterInventory {
+// Meghan Flaherty & Adam Curtin
+// CS145
+// Letter Inventory [lab 3]
+// this program can be used to keep track of an inventory of letters
+import java.util.*;
+
+
+
+public class LetterInventory extends LetterInventoryTest{
     private static final int ALPHABET_SIZE = 26;
     private int[] counts;
     private int size;
 
+    // constructor for the class
     public LetterInventory(String data) {
         counts = new int[ALPHABET_SIZE];
         for (char ch : data.toCharArray()) {
@@ -13,6 +22,55 @@ public class LetterInventory {
         }
     }
 
+    public static void prompt() {
+        Scanner input = new Scanner(System.in);
+            
+          //  LetterInventory inventory = new LetterInventory(input);
+            System.out.println("Would you like to enter a word in our inventory?");
+            System.out.println("please type:");
+            System.out.println("Yes - enter a word || No - quit this program");
+            String choice = input.next();
+            choice = choice.toLowerCase();
+            // LetterInventory inventory = new LetterInventory(input);
+            if (choice.equals("yes")) {
+               Execute(); 
+               
+            }
+            else if (choice.equals("no")) {
+                System.exit(0);
+            }
+    }
+
+    public static void Execute() {
+        System.out.print("Enter a word or sentence: ");
+        Scanner scan = new Scanner(System.in);
+        String word = scan.nextLine();
+        LetterInventory inventory = new LetterInventory(word);
+        System.out.println("Original string: " + word);
+        System.out.println("Sorted string: " + inventory.toString());
+        System.out.println("Letter count: " + inventory.size());
+        System.out.println("Empty? " + inventory.isEmpty());
+           promptAgain();
+    }
+
+    public static void promptAgain() {
+        Scanner inputTwo = new Scanner(System.in);
+        System.out.println("Would you like to enter another word?");
+        System.out.println("Yes / No");
+        String choiceTwo = inputTwo.next();
+        choiceTwo = choiceTwo.toLowerCase();
+        // LetterInventory inventory = new LetterInventory(input);
+        if (choiceTwo.equals("yes")) {
+           Execute(); 
+           
+        }
+        else if (choiceTwo.equals("no")) {
+            System.exit(0);
+        }
+}
+    
+
+    // get method for letter
     public int get(char letter) {
         if (!Character.isLetter(letter)) {
             throw new IllegalArgumentException("Not a letter: " + letter);
@@ -20,6 +78,7 @@ public class LetterInventory {
         return counts[Character.toLowerCase(letter) - 'a'];
     }
 
+    // set method for letter and value
     public void set(char letter, int value) {
         if (!Character.isLetter(letter)) {
             throw new IllegalArgumentException("Not a letter: " + letter);
@@ -32,14 +91,17 @@ public class LetterInventory {
         counts[index] = value;
     }
 
+    // returns size
     public int size() {
         return size;
     }
-
+    
+    // returns true if empty + false if not 
     public boolean isEmpty() {
         return size == 0;
     }
 
+    // to string method to build the string + format
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("[");
@@ -51,7 +113,8 @@ public class LetterInventory {
         sb.append("]");
         return sb.toString();
     }
-
+    
+    // adding a letter inventory 
     public LetterInventory add(LetterInventory other) {
         LetterInventory sum = new LetterInventory("");
         for (int i = 0; i < ALPHABET_SIZE; i++) {
@@ -61,6 +124,7 @@ public class LetterInventory {
         return sum;
     }
 
+    // taking out a letter inventory 
     public LetterInventory subtract(LetterInventory other) {
         LetterInventory diff = new LetterInventory("");
         for (int i = 0; i < ALPHABET_SIZE; i++) {
@@ -72,4 +136,5 @@ public class LetterInventory {
         }
         return diff;
     }
-} // end LetterInventory
+
+}
--- LetterInventoryTest.java
+++ LetterInventoryTest.java
@@ -1,41 +1,14 @@
+// Letter Inventory lab
+// Meghan Flaherty & Adam Curtin
+// this class executes the methods in letter inventory + prints accordingly
 
-    import java.util.Scanner;
+import java.util.Scanner;
     
+    // main class
     public class LetterInventoryTest {
         public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a word or sentence: ");
-        String input = scanner.nextLine();
-        LetterInventory inventory = new LetterInventory(input);
-        System.out.println("Original string: " + input);
-        System.out.println("Sorted string: " + inventory.toString());
-        System.out.println("Letter count: " + inventory.size());
-        System.out.println("Empty? " + inventory.isEmpty());
-            LetterInventory inventory1 = new LetterInventory("Hello");
-            System.out.println("Inventory 1: " + inventory1); // prints [ehllo]
-    
-            LetterInventory inventory2 = new LetterInventory("World");
-            System.out.println("Inventory 2: " + inventory2); // prints [dlorw]
-    
-            LetterInventory sum = inventory1.add(inventory2);
-            System.out.println("Sum: " + sum); // prints [dehllloorw]
-    
-            LetterInventory diff = inventory2.subtract(inventory1);
-            System.out.println("Difference: " + diff); // prints [dorw]
-    
-            // Test get() method
-            System.out.println("Number of 'l's in inventory 1: " + inventory1.get('l')); // prints 2
-            System.out.println("Number of 'z's in inventory 2: " + inventory2.get('z')); // throws IllegalArgumentException
-    
-            // Test set() method
-            inventory1.set('l', 3);
-            System.out.println("Inventory 1 after setting 'l' count to 3: " + inventory1); // prints [ehlloo]
-            inventory1.set('z', 2); // throws IllegalArgumentException
-    
-            // Test size() and isEmpty() methods
-            System.out.println("Size of inventory 2: " + inventory2.size()); // prints 5
-            System.out.println("Is inventory 1 empty? " + inventory1.isEmpty()); // prints false
-    
+           LetterInventory.prompt();
+
         } // end main method
     } // end test class
-    
+
